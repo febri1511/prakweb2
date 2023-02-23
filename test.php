f6e0b423f3e444925073eac90853b3a9742c42a1
febri1511 <febri15112003@gmail.com>
2023-02-23 15:32:38 +0700

    menambahkan folder baru

---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+// definisikan variable
+$nama = 'febrianscah';
+$umur = 18;
+$berat = 53;
+
+echo "Nama saya : $nama";
+echo '<br/>Umur saya : ' .$umur. ' Tahun';
+echo '<br/>Berat badan saya : ' .$berat. ' Kg';
+?>
